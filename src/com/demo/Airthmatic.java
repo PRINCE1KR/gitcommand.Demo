528fe9b2f1fd9317ea3874cb266bf991cff21c68
PRINCE1KR <prince.kumar1@nexturn.com>
2023-01-25 12:36:04 +0530

    push from intellij

---
+++ src/com/demo/Airthmatic.java
@@ -0,0 +1,25 @@
+package com.demo;
+
+public class Airthmatic {
+    public int add(int a,int b){
+        return a+b;
+    }
+    public int multiply(int a,int b){
+        return a*b;
+    }
+    public int substract(int a,int b){
+        return a-b;
+    }
+    public float division(int a,int b){
+        return a/b;
+    }
+
+
+    public static void main(String[] args) {
+        Airthmatic airthmatic = new Airthmatic();
+        System.out.println("adding two number-"+airthmatic.add(  2, 3 ));
+        System.out.println("multiply of two number-"+airthmatic.multiply(2,3));
+        System.out.println("substract of two number-"+airthmatic.substract(4,3));
+        System.out.println("division of two number-"+airthmatic.division(10,5));
+    }
+}
