f531d5e14e6a6ec6dea4390365881cc46d2fe041
PRINCE1KR <prince.kumar1@nexturn.com>
2023-01-28 12:02:43 +0530

    new practise

---
+++ src/com.demo/demo1/HelloPrince/src/HelloPrince.java
@@ -0,0 +1,8 @@
+import java.sql.SQLOutput;
+
+public class HelloPrince {
+    public static void main(String[] args) {
+        System.out.println("hello prince");
+        System.out.println("hello prince");
+    }
+}
