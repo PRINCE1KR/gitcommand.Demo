6c7485a5b6a7d326a04c0fa19b8ddb879a1309a1
PRINCE1KR <prince.kumar1@nexturn.com>
2023-01-28 12:07:55 +0530

    added 2 lines

--- src/com.demo/demo1/HelloPrince/src/HelloPrince.java
+++ src/com.demo/demo1/HelloPrince/src/HelloPrince.java
@@ -4,5 +4,8 @@ public class HelloPrince {
     public static void main(String[] args) {
         System.out.println("hello prince");
         System.out.println("hello prince");
+        System.out.println("hello prce arrya");
+        System.out.println("hello prce arrya");
+
     }
 }
